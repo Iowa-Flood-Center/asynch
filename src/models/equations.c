c0b3ef92e4391d27694ac741d33a48518631e914
Felipe Quintero <fquintero@gmail.com>
2022-11-03 12:54:53 -0500

    debug405

--- src/models/equations.c
+++ src/models/equations.c
@@ -3523,10 +3523,10 @@ void model405(double t, \
             // find the upper and lower storage threshold for this link
             double low_storage_threshold = max_storage_pond * factor_low_threshold;
             double high_storage_threshold = max_storage_pond * factor_high_threshold;
-            if(debug) printf("max.sto.pond %d\n",max_storage_pond);
-            if(debug) printf("low_storage_factor and high_storage_factor: %d and %d\n", factor_low_threshold,factor_high_threshold);
+            if(debug) printf("max.sto.pond %f\n",max_storage_pond);
+            if(debug) printf("low_storage_factor and high_storage_factor: %f and %f\n", factor_low_threshold,factor_high_threshold);
 
-            if(debug) printf("low_storage_threshold and high_storage_threshold: %d and %d\n", low_storage_threshold,high_storage_threshold);
+            if(debug) printf("low_storage_threshold and high_storage_threshold: %f and %f\n", low_storage_threshold,high_storage_threshold);
             if (dam_storage + dam_input >= max_storage_pond)
             {
                 if(debug) printf("storage >= max.sto.pond\n");
